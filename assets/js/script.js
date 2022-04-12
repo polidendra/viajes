3d7c15385f03a67a4a0e9118642be309cbc4810a
Leonor Lloyd <polalloyd@gmail.com>
2022-04-11 22:56:23 -0400

    agregando Javascript

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+
+        $(document).ready(function () {
+            $("#boton").click(function () {
+                 $(this).html(alert("El formulario fue enviado correctamente.."));
+            });
+        });
+        $(document).ready(function () {
+            $("h3").dblclick(function () {
+                $(this).css({
+                    "color": "red",
+                });
+            }); 
+        });
+        $(document).ready(function () {
+           $(".cardhide").click(function() {
+              $(".divhide").toggle();
+          })
+        });
